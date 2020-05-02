1d8f039e52d46c1a010569bd6bf424f8e018cfa2
Henrynunez112 <henrynunez@pursuit.org>
2020-05-02 18:51:49 -0400

    .env configurated

--- react-weather-app/src/components/WeatherCard/WeatherDiv.js
+++ react-weather-app/src/components/WeatherCard/WeatherDiv.js
@@ -3,17 +3,20 @@ import WeatherDetails from "./WeatherDetails/WeatherDetails";
 import assetMapping from "../../assets/assetMapping.json";
 import Header from "../Header/Header";
 import axios from "axios";
+const { REACT_APP_APIKEY } = process.env;
 
 const WeatherDiv = () => {
   const [search, setSearch] = useState("");
-  const [weather, setWeather] = useState({ temperature: "", description: "", color: "" });
+  const [weather, setWeather] = useState({
+    temperature: "",
+    description: "",
+    color: "",
+  });
   const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false)
 
   const handleClick = async () => {
-    const API_KEY = "3382429b47bd7f5f9290fad35b01287d";
     const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-    const URL = API_URL + `?q=${search}&appid=${API_KEY}&units=metric`;
+    const URL = API_URL + `?q=${search}&appid=${REACT_APP_APIKEY}&units=metric`;
     try {
       const res = await axios.get(URL);
       if (res.data.cod === 200) {
@@ -21,7 +24,7 @@ const WeatherDiv = () => {
           ...weather,
           description: res.data.weather[0].description,
           temperature: res.data.main.temp,
-          color: res.data.weather[0].main
+          color: res.data.weather[0].main,
         });
       } else {
         throw res.data.cod;
@@ -29,16 +32,13 @@ const WeatherDiv = () => {
     } catch (err) {
       console.log(err);
       setError(true);
-      setLoading(false)
     }
   };
 
   return (
     <div className="">
       <Header
-        color={
-          assetMapping.colors[error === true ? "error" : weather.color]
-        }
+        color={assetMapping.colors[error === true ? "error" : weather.color]}
       />
       <main>
         <input value={search} onChange={(e) => setSearch(e.target.value)} />
